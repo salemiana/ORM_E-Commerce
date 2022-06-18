efe18e0c55657e71ab33b467bc1884ad65bd11d4
salem z <salem.zerezghi2@gmail.com>
2022-06-17 18:57:19 -0600

    defined all columns

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,38 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define a product_name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // define price column
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isPrice: true},
+    },
+    // define a stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [10]},
+    },
+    // define category_id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      //References the category model's id
+    }
+    
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,8 +6,25 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // define id columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define product_id
+    product_id: {
+      type: DataTypes.INTEGER,
+      //References the product model's id
+    },
+    // define tag_id columns
+    tag_id: {
+      type:DataTypes.INTEGER
+      //References the product model's id
+    },
   },
+  
   {
     sequelize,
     timestamps: false,
